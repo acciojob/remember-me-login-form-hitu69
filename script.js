a7d81ec37b8caf98a48ae7df929242d9686a85a1
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-23 11:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const form = document.querySelector('form');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
@@ -7,33 +6,33 @@ const existingButton = document.getElementById('existing');
 
 // On page load, check if there are stored login details
 if (localStorage.getItem('username') && localStorage.getItem('password')) {
-  // Populate the input fields with the stored login details
+// Populate the input fields with the stored login details
 
-  // Show the "Login as existing user" button
-  existingButton.style.display = 'block';
+// Show the "Login as existing user" button
+existingButton.style.visiblity = "visible"
 }
 
 // Add an event listener to the form on submit
 form.addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent the form from submitting
+e.preventDefault(); // Prevent the form from submitting
 
-  const username = usernameInput.value;
-  const password = passwordInput.value;
+const username = usernameInput.value;
+const password = passwordInput.value;
 
-  // Check if the "remember me" checkbox is checked
-  if (rememberCheckbox.checked) {
-    // Store the username and password in local storage
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-  } else {
-    // Remove the stored username and password from local storage
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
-  }
+// Check if the "remember me" checkbox is checked
+if (rememberCheckbox.checked) {
+// Store the username and password in local storage
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+} else {
+// Remove the stored username and password from local storage
+localStorage.removeItem('username');
+localStorage.removeItem('password');
+}
 
-  // TODO: Add your login logic here
-	alert(`Logged in as ${usernameInput.value}`)
+// TODO: Add your login logic here
+alert(Logged in as ${usernameInput.value})
 
-  // Show the "Login as existing user" button
-  existingButton.style.display = 'block';
+// Show the "Login as existing user" button
+existingButton.style.display = 'block';
 });
